53a851e1f1405f17a5e6e1ec18bbff6d82f709a1
Hryhorii Khyzhniakov <soults15@gmail.com>
2025-07-21 10:41:29 +0300

    chore: init project

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginNodeImport from 'eslint-plugin-node-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+
+  {
+    plugins: { 'node-import': eslintPluginNodeImport },
+    rules: {
+      'node-import/prefer-node-protocol': 'error',
+    },
+  },
+
+  {
+    plugins: { prettier: eslintPluginPrettier },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  }
+);
