81a0d716850f06c8c2318cabdab1b0b3d2ec2cb2
Hryhorii Khyzhniakov <soults15@gmail.com>
2025-07-23 13:23:24 +0300

    deploy: containerization and replicas

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileEntity } from '~/files/entities/file.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'google-uploader',
-      entities: [FileEntity],
-      synchronize: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST') as string,
+        port: 5432,
+        username: 'root',
+        password: 'root',
+        database: 'google-uploader',
+        entities: [FileEntity],
+        synchronize: true,
+      }),
     }),
   ],
 })
--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,14 +1,18 @@
 import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { QueueNames } from '~/queue/constants';
 
 @Module({
   imports: [
-    BullModule.forRoot({
-      connection: {
-        host: 'localhost',
-        port: 6379,
-      },
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get('REDIS_HOST'),
+          port: 6379,
+        },
+      }),
     }),
     BullModule.registerQueue({ name: QueueNames.DOWNLOAD_AND_UPLOAD }),
   ],
